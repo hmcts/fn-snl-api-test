5d64fcc9e9faf18a177253af097447fd4672e4ab
Pettedson John <pettedson.john@hmcts.net>
2021-01-22 12:27:55 +0000

    Added all of the GETSessions Tests.....

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/snl/acceptance/sessions/GETSessionsPayloadValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/snl/acceptance/sessions/GETSessionsPayloadValidationTest.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.futurehearings.snl.Application;
 import uk.gov.hmcts.futurehearings.snl.acceptance.common.TestingUtils;
 import uk.gov.hmcts.futurehearings.snl.acceptance.common.delegate.dto.DelegateDTO;
 import uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.dto.SNLVerificationDTO;
+import uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.success.SNLCommonSuccessVerifier;
 import uk.gov.hmcts.futurehearings.snl.acceptance.sessions.dto.SessionsVerificationDTO;
 import uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.error.SNLCommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.snl.acceptance.sessions.verify.GETSessionsPayloadValidationVerifier;
@@ -15,9 +16,12 @@ import java.util.Map;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
@@ -50,33 +54,20 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
         this.setHttpMethod(HttpMethod.GET);
         this.setHttpSuccessStatus(HttpStatus.OK);
         this.setRelativeURLForNotFound(this.getRelativeURL().replace("sessions", "session"));
-        this.setSnlSuccessVerifier(new GETSessionsPayloadValidationVerifier());
+        this.setSnlSuccessVerifier(new SNLCommonSuccessVerifier());
         this.setSnlErrorVerifier(new SNLCommonErrorVerifier());
         this.setInputFileDirectory("sessions");
         this.setInputPayloadFileName("empty-json-payload.json");
         TestingUtils.readFileContents(String.format(INPUT_FILE_PATH, getInputFileDirectory()) +
                 "/" + getInputPayloadFileName());
-        Map<String, String> urlParams = Map.of("requestSessionType", "ADHOC");
-        this.setUrlParams(urlParams);
     }
 
     @Test
     @DisplayName("Successfully validated response with all the header values - Query Param : requestSessionType=ADHOC")
     public void test_successful_response_with_a_complete_header() throws Exception {
 
-        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
-                createCompletePayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
-        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
-        sessionsVerificationDTO.requestSessionType("ADHOC");
-        commonDelegate.test_expected_response_for_supplied_header(
-                delegateDTO,
-                new GETSessionsPayloadValidationVerifier(), sessionsVerificationDTO);
-    }
-
-    @Test
-    @DisplayName("Successfully validated response with all the header values - Query Param : requestSessionType=ADHOC")
-    public void test_negative_response_with_a_mandatory_header() throws Exception {
-
+        Map<String, String> urlParams = Map.of("requestSessionType", "ADHOC");
+        this.setUrlParams(urlParams);
         DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
                 createCompletePayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
         SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
@@ -90,6 +81,8 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
     @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC")
     public void test_successful_response_with_a_mandatory_header() throws Exception {
 
+        Map<String, String> urlParams = Map.of("requestSessionType", "ADHOC");
+        this.setUrlParams(urlParams);
         DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
                 createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
         SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
@@ -97,6 +90,22 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
         commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
                 new GETSessionsPayloadValidationVerifier(),
                 sessionsVerificationDTO);
+    }
+
+    @DisplayName("Negative response with mandatory header values Param : {0} --> {1}")
+    @ParameterizedTest(name = "caseIDHMCTS Negative tests")
+    @CsvSource(value = {"Empty Space,''", "Blank Value Space,' '", "Invalid Look Up,HOC"}, nullValues = "NIL")
+    public void test_negative_response_with_a_mandatory_header_invalid_session_type(final String key, final String value) throws Exception {
+
+        Map<String, String> urlParams = Map.of("requestSessionType", value);
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
+        sessionsVerificationDTO.requestSessionType(value);
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                this.getSnlErrorVerifier(),
+                new SNLVerificationDTO(HttpStatus.BAD_REQUEST, "1000", "'" + value + "' is not a valid value for field 'requestSessionType'", null));
     }
 
     @Test
@@ -117,19 +126,37 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
                 sessionsVerificationDTO);
     }
 
-    @Test
-    @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC, requestJudgeType=DJCC")
-    public void test_successful_response_with_a_mandatory_header_and_request_judge_type() throws Exception {
+    @DisplayName("Negative response with mandatory header values Param : {0} --> {1}")
+    @ParameterizedTest(name = "Invalid locationDuration")
+    @CsvSource(value = {"Empty Space,''", "Invalid Request Duration,0", "Invalid Request Duration,-1"}, nullValues = "NIL")
+    //TODO - Raise Defects for "Blank Value Space,' '"  as it was throwing a 404 HTTP Status for this Condition
+    public void test_negative_response_with_a_mandatory_header_invalid_duration(final String key, final String value) throws Exception {
 
         Map<String, String> urlParams = Map.of(
                 "requestSessionType", "ADHOC",
-                "requestJudgeType", "DJCC");
+                "requestDuration", value);
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                this.getSnlSuccessVerifier(),
+                new SNLVerificationDTO(HttpStatus.OK, null, null, null));
+    }
+
+    @ParameterizedTest(name = "Valid Request Judge Type")
+    @DisplayName("Successfully validated response with mandatory header values - Query Param : {0} --> {1}")
+    @CsvSource(value = {"Valid Values,PUBLAW", "Valid Values,DJCC"}, nullValues = "NIL")
+    public void test_successful_response_with_a_mandatory_header_and_request_judge_type(final String key, final String value) throws Exception {
+
+        Map<String, String> urlParams = Map.of(
+                "requestSessionType", "ADHOC",
+                "requestJudgeType", value);
         this.setUrlParams(urlParams);
         DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
                 createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
         SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
         sessionsVerificationDTO.requestSessionType("ADHOC");
-        sessionsVerificationDTO.requestJudgeType("DJCC");
+        sessionsVerificationDTO.requestJudgeType( value);
         commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
                 new GETSessionsPayloadValidationVerifier(),
                 sessionsVerificationDTO);
@@ -137,7 +164,7 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
 
     @Test
     @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC, requestLocationId=301")
-    //TODO - Only working for Room Id's and not for any Venue Id's....
+    //TODO - Riase a Defect only working for Room Id's and not for any Venue Id's....
     public void test_successful_response_with_a_mandatory_header_and_request_location_id() throws Exception {
 
         Map<String, String> urlParams = Map.of(
@@ -154,13 +181,30 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
                 sessionsVerificationDTO);
     }
 
+    @DisplayName("Negative response with mandatory header values Param : {0} --> {1}")
+    @ParameterizedTest(name = " requestLocationId Negative tests")
+    @CsvSource(value = {"Invalid Location Id,299", "Invalid Location Id,0","Invalid Location Id,-1"}, nullValues = "NIL")
+    public void test_negative_response_with_a_mandatory_header_and_request_location_id(final String key, final String value) throws Exception {
+
+        Map<String, String> urlParams = Map.of("requestSessionType", "ADHOC",
+                                                "requestLocationId",value);
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
+        sessionsVerificationDTO.requestSessionType(value);
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                this.getSnlErrorVerifier(),
+                new SNLVerificationDTO(HttpStatus.BAD_REQUEST, "1002", "'" + value + "' is not a valid value for parameter 'requestLocationId'", null));
+    }
+
     @Test
     @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC, requestStartDate=2020-12-09T10:00:00Z")
     public void test_successful_response_with_a_mandatory_header_and_request_start_date() throws Exception {
 
         Map<String, String> urlParams = Map.of(
                 "requestSessionType", "ADHOC",
-                "requestStartDate", "2020-12-9T10:00:00Z");
+                "requestStartDate", "2020-12-09T10:00:00Z");
         this.setUrlParams(urlParams);
         DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
                 createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
@@ -172,13 +216,33 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
                 sessionsVerificationDTO);
     }
 
+    @DisplayName("Negative response with mandatory header values Param : {0} --> {1}")
+    @ParameterizedTest(name = " requestStartDate Negative tests")
+    @CsvSource(value = {"Invalid Request Start Date Format,2020-12-09T10:00:00.123Z",}, nullValues = "NIL")
+    //TODO - Raise a Defect for the condition "Invalid Request Start Date Format,2020-12-9T10:00:00Z" as it does not seem to format these date types
+    public void test_negative_response_with_a_mandatory_header_and_request_start_date(final String key, final String value) throws Exception {
+
+        Map<String, String> urlParams = Map.of(
+                "requestSessionType", "ADHOC",
+                "requestStartDate", value);
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
+        sessionsVerificationDTO.requestSessionType("ADHOC");
+        sessionsVerificationDTO.requestStartDate(value);
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                this.getSnlErrorVerifier(),
+                new SNLVerificationDTO(HttpStatus.BAD_REQUEST, "9999", "Date '"+value+"' not in RFC 3339 format", null));
+    }
+
     @Test
     @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC, requestEndDate=2020-12-09T10:00:00Z")
     public void test_successful_response_with_a_mandatory_header_and_request_end_date() throws Exception {
 
         Map<String, String> urlParams = Map.of(
                 "requestSessionType", "ADHOC",
-                "requestEndDate", "2020-12-9T10:00:00Z");
+                "requestEndDate", "2020-12-09T10:00:00Z");
         this.setUrlParams(urlParams);
         DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
                 createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
@@ -188,6 +252,26 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
         commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
                 new GETSessionsPayloadValidationVerifier(),
                 sessionsVerificationDTO);
+    }
+
+    @DisplayName("Negative response with mandatory header values Param : {0} --> {1}")
+    @ParameterizedTest(name = " requestEndDate Negative tests")
+    @CsvSource(value = {"Invalid Request End Date Format,2020-12-09T10:00:00.123Z",}, nullValues = "NIL")
+    //TODO - Raise a Defect for the condition "Invalid Request Start Date Format,2020-12-9T10:00:00Z" as it does not seem to format these date types
+    public void test_negative_response_with_a_mandatory_header_and_request_end_date(final String key, final String value) throws Exception {
+
+        Map<String, String> urlParams = Map.of(
+                "requestSessionType", "ADHOC",
+                "requestEndDate", value);
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
+        sessionsVerificationDTO.requestSessionType("ADHOC");
+        sessionsVerificationDTO.requestEndDate(value);
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                this.getSnlErrorVerifier(),
+                new SNLVerificationDTO(HttpStatus.BAD_REQUEST, "9999", "Date '"+value+"' not in RFC 3339 format", null));
     }
 
     @Test
@@ -203,6 +287,49 @@ public class GETSessionsPayloadValidationTest extends SessionsPayloadValidationT
                 createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
         SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
         sessionsVerificationDTO.requestSessionType("ADHOC");
+        sessionsVerificationDTO.requestStartDate("2020-12-01T10:00:00Z");
+        sessionsVerificationDTO.requestEndDate("2020-12-09T10:00:00Z");
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                new GETSessionsPayloadValidationVerifier(),
+                sessionsVerificationDTO);
+    }
+
+    @Test
+    @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC, requestStartDate=2020-12-01T10:00:00Z , requestStartDate=2020-12-09T10:00:00Z")
+    public void test_successful_response_with_a_mandatory_header_between_location_and_judge_types() throws Exception {
+
+        Map<String, String> urlParams = Map.of(
+                "requestSessionType", "ADHOC",
+                "requestJudgeType", "CHC",
+                "requestLocationId", "360");
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(),
+                null, null, null);
+        sessionsVerificationDTO.requestSessionType("ADHOC");
+        sessionsVerificationDTO.requestJudgeType("CHC");
+        sessionsVerificationDTO.requestLocationID("360");
+        commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
+                new GETSessionsPayloadValidationVerifier(),
+                sessionsVerificationDTO);
+    }
+
+    @Test
+    @DisplayName("Successfully validated response with mandatory header values - Query Param : requestSessionType=ADHOC, requestStartDate=2020-12-01T10:00:00Z , requestStartDate=2020-12-09T10:00:00Z")
+    public void test_successful_response_with_a_mandatory_header_between_start_and_end_dates_and_duration() throws Exception {
+
+        Map<String, String> urlParams = Map.of(
+                "requestSessionType", "ADHOC",
+                "requestStartDate", "2020-12-01T10:00:00Z",
+                "requestDuration", "360",
+                "requestEndDate", "2020-12-09T10:00:00Z");
+        this.setUrlParams(urlParams);
+        DelegateDTO delegateDTO = buildDelegateDTO(getRelativeURL(),
+                createStandardPayloadHeader(getApiSubscriptionKey()), getHttpMethod(), getHttpSuccessStatus());
+        SessionsVerificationDTO sessionsVerificationDTO = new SessionsVerificationDTO(getHttpSuccessStatus(), null, null, null);
+        sessionsVerificationDTO.requestSessionType("ADHOC");
+        sessionsVerificationDTO.requestDuration("360");
         sessionsVerificationDTO.requestStartDate("2020-12-01T10:00:00Z");
         sessionsVerificationDTO.requestEndDate("2020-12-09T10:00:00Z");
         commonDelegate.test_expected_response_for_supplied_header(delegateDTO,
--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/snl/acceptance/sessions/verify/GETSessionsPayloadValidationVerifier.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/snl/acceptance/sessions/verify/GETSessionsPayloadValidationVerifier.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.futurehearings.snl.acceptance.sessions.verify;
 
-import static com.jayway.jsonpath.JsonPath.read;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import uk.gov.hmcts.futurehearings.snl.acceptance.common.dto.SNLDto;
-import uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.dto.SNLVerificationDTO;
 import uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.success.SNLSuccessVerifier;
 import uk.gov.hmcts.futurehearings.snl.acceptance.sessions.dto.SessionsVerificationDTO;
 
@@ -51,9 +49,10 @@ public class GETSessionsPayloadValidationVerifier implements SNLSuccessVerifier 
                 && sessionsVerificationDTO.requestSessionType() != null
                 && !sessionsVerificationDTO.requestSessionType().trim().equals("")) {
 
+            final int requestDuration = Integer.parseInt(sessionsVerificationDTO.requestDuration());
             final List<Integer> sessionsList = context.read("$.sessionsResponse.sessions[*].sessionDuration");
             assertTrue(sessionsList.stream().allMatch(stringObjectMap -> {
-                return stringObjectMap == 360;
+                return stringObjectMap == requestDuration;
             }));
             final List<String> sessionTypeList = context.read(
                     "$.sessionsResponse.sessions[*].sessionType");
@@ -68,8 +67,9 @@ public class GETSessionsPayloadValidationVerifier implements SNLSuccessVerifier 
                 && !sessionsVerificationDTO.requestSessionType().trim().equals("")) {
 
             final List<String> sessionsList = context.read("$.sessionsResponse.sessions[*].sessionJudges[*].sessionJudgeType");
+            final String requestJudgeType = sessionsVerificationDTO.requestJudgeType();
             assertTrue(sessionsList.stream().allMatch(stringObjectMap -> {
-                return stringObjectMap.equals("DJCC");
+                return stringObjectMap.equals(requestJudgeType);
             }));
             final List<String> sessionTypeList = context.read(
                     "$.sessionsResponse.sessions[*].sessionType");
@@ -150,12 +150,73 @@ public class GETSessionsPayloadValidationVerifier implements SNLSuccessVerifier 
                     DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
             log.debug("The value of the given date" + dateTimeEndDateInput);
             log.debug("The value of the given date seconds" + dateTimeEndDateInput.getSecond());
-            final List<String> sessionsList = context.read("$.sessionsResponse.sessions[*].sessionEndTime");
+            final List<String> sessionsList = context.read("$.sessionsResponse.sessions[*].sessionStartTime");
             assertTrue(sessionsList.stream().allMatch(s -> {
                 final LocalDateTime dateTime = LocalDateTime.parse(s,
                         DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
                 return (dateTime.isAfter(dateTimeStartDateInput) && dateTime.isBefore(dateTimeEndDateInput)
-                        || (dateTime.isEqual(dateTimeStartDateInput) || dateTime.isEqual(dateTimeEndDateInput))) ;
+                        || (dateTime.isEqual(dateTimeStartDateInput) || dateTime.isEqual(dateTimeEndDateInput)));
+            }));
+            final List<String> sessionTypeList = context.read(
+                    "$.sessionsResponse.sessions[*].sessionType");
+
+            assertTrue(sessionTypeList.stream().allMatch(s -> {
+                return s.equals("ADHOC");
+            }));
+        } else if (sessionsVerificationDTO.requestSessionType() != null
+                && !sessionsVerificationDTO.requestSessionType().trim().equals("")
+                && sessionsVerificationDTO.requestJudgeType() != null
+                && !sessionsVerificationDTO.requestJudgeType().trim().equals("")
+                && sessionsVerificationDTO.requestLocationID() != null
+                && !sessionsVerificationDTO.requestLocationID().trim().equals("")) {
+
+            final String requestJudgeType = sessionsVerificationDTO.requestJudgeType();
+            final String requestSessionType = sessionsVerificationDTO.requestSessionType();
+            final String requestLocationID = sessionsVerificationDTO.requestLocationID();
+
+            final List<String> sessionsJudgeTypeList = context.read("$.sessionsResponse.sessions[*].sessionJudgeType");
+            assertTrue(sessionsJudgeTypeList.stream().allMatch(s -> {
+                return s.equals(requestJudgeType);
+            }));
+
+            final List<String> sessionsLocationIdList = context.read("$.sessionsResponse.sessions[*].sessionRoomId");
+            assertTrue(sessionsJudgeTypeList.stream().allMatch(s -> {
+                return s.equals(requestLocationID);
+            }));
+
+            final List<String> sessionTypeList = context.read(
+                    "$.sessionsResponse.sessions[*].sessionType");
+
+            assertTrue(sessionTypeList.stream().allMatch(s -> {
+                return s.equals(requestSessionType);
+            }));
+        } else if (sessionsVerificationDTO.requestStartDate() != null
+                && !sessionsVerificationDTO.requestStartDate().trim().equals("")
+                && sessionsVerificationDTO.requestEndDate() != null
+                && !sessionsVerificationDTO.requestEndDate().trim().equals("")
+                && sessionsVerificationDTO.requestSessionType() != null
+                && !sessionsVerificationDTO.requestSessionType().trim().equals("")
+                && sessionsVerificationDTO.requestDuration() != null
+                && !sessionsVerificationDTO.requestDuration().trim().equals("")) {
+
+            final int requestDuration = Integer.parseInt(sessionsVerificationDTO.requestDuration());
+            final List<Integer> sessionsDurationList = context.read("$.sessionsResponse.sessions[*].sessionDuration");
+            assertTrue(sessionsDurationList.stream().allMatch(stringObjectMap -> {
+                return stringObjectMap == requestDuration;
+            }));
+
+            final LocalDateTime dateTimeStartDateInput = LocalDateTime.parse(sessionsVerificationDTO.requestStartDate(),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+            final LocalDateTime dateTimeEndDateInput = LocalDateTime.parse(sessionsVerificationDTO.requestEndDate(),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+            log.debug("The value of the given date" + dateTimeEndDateInput);
+            log.debug("The value of the given date seconds" + dateTimeEndDateInput.getSecond());
+            final List<String> sessionsList = context.read("$.sessionsResponse.sessions[*].sessionStartTime");
+            assertTrue(sessionsList.stream().allMatch(s -> {
+                final LocalDateTime dateTime = LocalDateTime.parse(s,
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+                return (dateTime.isAfter(dateTimeStartDateInput) && dateTime.isBefore(dateTimeEndDateInput)
+                        || (dateTime.isEqual(dateTimeStartDateInput) || dateTime.isEqual(dateTimeEndDateInput)));
             }));
             final List<String> sessionTypeList = context.read(
                     "$.sessionsResponse.sessions[*].sessionType");
@@ -164,5 +225,6 @@ public class GETSessionsPayloadValidationVerifier implements SNLSuccessVerifier 
                 return s.equals("ADHOC");
             }));
         }
+
     }
 }
